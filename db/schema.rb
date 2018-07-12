af1ee002d8021b7aec02f824526fbee1dd8fd251
kadenjenson <sobeit99@gmail.com>
2018-07-12 17:56:08 -0600

    Added row to students data table for future radio buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711195252) do
+ActiveRecord::Schema.define(version: 20180712184126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180711195252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "course_id"
+    t.string "course_select"
     t.index ["course_id"], name: "index_students_on_course_id"
   end
 
